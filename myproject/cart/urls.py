394770e194487e36d1dffb8f316eee3905998155
nomura3656 <138486255+nomura3656@users.noreply.github.com>
2023-07-31 13:19:38 +0000

    KKK

--- myproject/cart/urls.py
+++ myproject/cart/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('add_to_cart/<str:item>/', views.IndexView.as_view(), name='add_to_cart'),
     path('cart/', views.view_cart, name='cart'),
+    path('kill_session/', views.kill_session, name='kill_session'),
 ]
--- myproject/cart/views.py
+++ myproject/cart/views.py
@@ -11,18 +11,31 @@ class IndexView(View):
 
     def post(self, request, *args, **kwargs):
         item = request.POST.get('item', '')  # itemがない場合はデフォルト値を空文字に設定
-        self.add_to_cart(request, item)
+        if item == 'A':
+            test = [1, 1980, 'スニーカー', 'converse', 'man']
+        
+        if item == 'B':
+            test = [2, 980, 'サンダル', 'クロックス', 'unisex']
+        
+        self.add_to_cart(request, test)
         print(request.session['cart'])
         return render(request, "index.html")
 
-    def add_to_cart(self, request, item):
+    def add_to_cart(self, request, test):
         if 'cart' not in request.session:
             request.session['cart'] = []
         my_list = request.session.get('cart', [])
         # リストに要素を追加する
-        my_list.append(item)
+        my_list.append(test)
         # セッションにリストを保存する
         request.session['cart'] = my_list
 def view_cart(request):
     cart_items = request.session.get('cart')
     return render(request, 'cart/view_cart.html', {'cart_items': cart_items})
+
+def kill_session(request):
+    request.session.flush()
+    print("sessionを消した")
+    return render(request, "index.html")
+
+
